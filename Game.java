9cee0a79bd68901157af506a5eb1abcc9c0335f6
MichaelPoe44 <michaelcp387@gmail.com>
2024-06-20 15:40:08 -0500

    deleted

--- Game.java
+++ Game.java
@@ -1,15 +1,8 @@
-import javax.swing.SwingUtilities;
 
 public class Game {
 
     public static void main(String[] args){
-        // SwingUtilities.invokeLater(new Runnable() {
-        //     @Override
-        //     public void run(){ 
-        //         MainWindow game = new MainWindow();
-        //         game.show();
-        //     }
-        // });
+        
         
        MainWindow game = new MainWindow();
         
